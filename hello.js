6ef02dd90f3d82e622981cd69f341f6a4219d73d
NorwegianKiwi-glitch <simonhelgen1@gmail.com>
2022-10-26 21:24:36 +0200

    oppdatertKode

--- hello.js
+++ hello.js
@@ -5,3 +5,7 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
     output.innerHTML = this.value;
 }
+
+fetch('https://bacit.info/hwr/20210926134925-47352fbdecddd9a754001f4e4abbf40f.json')
+    .then(response => response.json())
+    .then(data => console.log(data));
